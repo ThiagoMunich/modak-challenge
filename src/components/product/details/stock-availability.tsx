6f271e541d8281b2eeb4408e26d7eba4ab7e8e8e
Thiago Munich <thiago.munich@direcional.com.br>
2025-04-10 23:32:56 -0300

    feat: refactor StockAvailability component to improve layout and encapsulate stock display logic in a reusable Box component

--- src/components/product/details/stock-availability.tsx
+++ src/components/product/details/stock-availability.tsx
@@ -1,11 +1,10 @@
-import React from "react"
+import React, { ReactNode } from "react"
 
-import { Button, Text, View } from "react-native"
+import { Text, View } from "react-native"
 
 import Animated, { FadeInDown, SlideInDown } from "react-native-reanimated"
 
 import { Product } from "@/@types/products-types"
-import { ThemedButton } from "@/components/shared/button"
 
 type Props = Pick<Product, "stock">
 
@@ -19,27 +18,37 @@ export function StockAvailability({ stock }: Props) {
   )
 }
 
+function Box({ children }: { children: ReactNode }) {
+  return (
+    <View className="items-center py-5 border-2 border-dashed border-red-700 rounded-full shadow shadow-red-700">
+      {children}
+    </View>
+  )
+}
+
 function CurrentStock({ stock }: Props) {
   return (
-    <>
-      <Text className="text-gray-800 text-lg text-center">ONLY</Text>
+    <Box>
+      <Text className="absolute font-black -top-5 text-3xl shadow shadow-red-700 text-gray-800 text-center">ONLY</Text>
       <Text className="text-red-700 text-[160px] text-center font-black">{stock}</Text>
-      <Text className="text-gray-800 text-lg text-center">REMAINING</Text>
-    </>
+      <Text className="absolute font-black -bottom-5 text-black shadow shadow-red-700 text-3xl text-center">
+        REMAINING
+      </Text>
+    </Box>
   )
 }
 
 function NoStock() {
   return (
-    <View className="items-center py-5 border-2 border-dashed border-red-700">
+    <Box>
       <Animated.Text
         entering={SlideInDown.springify().mass(0.4).delay(200)}
-        className="absolute right-0 -top-10 text-6xl py-3 -rotate-45"
+        className="absolute right-0 -top-10 text-6xl py-3 -rotate-45 shadow shadow-red-700"
       >
         🚀
       </Animated.Text>
       <Text className="text-gray-600">This item has just</Text>
       <Text className="text-red-700 text-center font-black text-4xl">SOLD OUT</Text>
-    </View>
+    </Box>
   )
 }
